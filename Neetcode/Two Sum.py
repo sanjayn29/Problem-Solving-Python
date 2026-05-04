e93d938b2130c19949e510656a71ab4ae2f4d420
Sanjay N <sanjayn29.aiml@gmail.com>
2026-05-04 23:30:23 +0530

    two sum

--- Neetcode/Two Sum.py
+++ Neetcode/Two Sum.py
@@ -1,7 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if ((nums[i]+nums[j])==target):
-                    return [i,j]
+            balance = target - nums[i]
+
+            if balance in seen:
+                return [seen[balance],i]
+
+            seen[nums[i]]=i
+
         return []
