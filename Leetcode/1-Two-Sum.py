4e4fc1a3f0b830d7352d4374e41a0e7e76a21a01
Sanjay N <sanjayn29.aiml@gmail.com>
2026-05-04 23:31:20 +0530

    two sum

--- Leetcode/1-Two-Sum.py
+++ Leetcode/1-Two-Sum.py
@@ -1,7 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        seen = {}
+
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if ((nums[i]+nums[j])==target):
-                    return [i,j]
+            balance = target - nums[i]
+
+            if balance in seen:
+                return [seen[balance],i]
+
+            seen[nums[i]]=i
+
         return []
