a8c2d5c3342d2f0e5591c828eb25e38a655da8f5
Sanjay N <sanjayn29.aiml@gmail.com>
2026-05-19 22:42:54 +0530

    Search in Rotated Sorted Array

---
+++ GFG/Search in Rotated Sorted Array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums, target):
+        # code here
+        left = 0
+        right = len(nums)-1
+        while(left <= right):
+            mid = left + (right-left) / 2
+            mid = int(mid)
+            if (nums[mid] == target):
+                return mid
+            if(nums[left] <= nums[mid]):
+                if(nums[left] <= target and target < nums[mid]):
+                    right=mid-1
+                else:
+                    left=mid+1
+            else:
+                if(nums[mid] < target and target <= nums[right]):
+                    left=mid+1
+                else:
+                    right=mid-1
+        return -1
