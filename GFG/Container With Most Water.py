ac0c8a8ff5e944c932963b53a1eb9215aa6823e9
Sanjay N <sanjayn29.aiml@gmail.com>
2026-05-23 22:06:12 +0530

    Container with water

---
+++ GFG/Container With Most Water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxWater(self, height):
+        # code here
+        left = 0
+        right = len(height)-1
+        maxy = 0
+        while( left < right):
+            width = right - left
+            hight = min(height[left],height[right])
+            area = width * hight
+            maxy = max(maxy,area)
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right-=1
+        return maxy
