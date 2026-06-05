c26ada03077fc92c1b30071e72477e11960f85d5
Sanjay N <sanjayn29.aiml@gmail.com>
2026-06-05 05:45:25 +0530

    generate paren

---
+++ Leetcode/22-Generate Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def backtracking(res,curr,openn,close,n):
+            if len(curr)==n*2:
+                res.append(curr)
+                return
+            if openn < n:
+                backtracking(res,curr+"(",openn+1,close,n)
+            if close < openn:
+                backtracking(res,curr+")",openn,close+1,n) 
+
+        backtracking(res, "",0,0,n)
+        return res        
---
+++ Neetcode/Generate Parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParentheses(self, n):
+        #code here
+        res = []
+
+        def backtracking(res,curr,openn,close,n):
+            if len(curr)==n*2:
+                res.append(curr)
+                return
+            if openn < n:
+                backtracking(res,curr+"(",openn+1,close,n)
+            if close < openn:
+                backtracking(res,curr+")",openn,close+1,n) 
+
+        backtracking(res, "",0,0,n)
+        return res
