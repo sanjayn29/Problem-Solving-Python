7a83c3ee3a92e5a69cca5e4c5b43ea7f22f47128
Sanjay N <sanjayn29.aiml@gmail.com>
2026-06-12 22:57:15 +0530

    nee sonna na podura okay va

---
+++ GFG/Set Matrix Zeros.py
@@ -0,0 +1,42 @@
+class Solution:
+    def setMatrixZeroes(self, matrix):
+        # code here
+        m = len(matrix)
+        n = len(matrix[0])
+
+        fr = False
+        fc = False
+
+        for i in range(m):
+            if matrix[i][0] == 0:
+                fc = True
+                break
+
+        for j in range(n):
+            if matrix[0][j] == 0:
+                fr = True
+                break
+
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in range(1,m):
+            if matrix[i][0] == 0:
+                for j in range(1,n):
+                    matrix[i][j] = 0
+
+        for j in range(1,n):
+            if matrix[0][j] == 0:
+                for i in range(1,m):
+                    matrix[i][j] = 0
+
+        if fr:
+            for j in range(n):
+                matrix[0][j] = 0
+
+        if fc:
+            for i in range(m):
+                matrix[i][0] = 0       
