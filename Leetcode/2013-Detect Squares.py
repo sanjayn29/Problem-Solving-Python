0a9b528e0fd0aff9e8592bbd0bdf6062fa306e93
Sanjay N <sanjayn29.aiml@gmail.com>
2026-06-24 15:28:54 +0530

    Detect Squares

---
+++ Leetcode/2013-Detect Squares.py
@@ -0,0 +1,19 @@
+class DetectSquares:
+
+    def __init__(self):
+        self.points = {}        
+
+    def add(self, point: List[int]) -> None:
+        key = (point[0],point[1])
+        self.points[key]=self.points.get(key,0)+1
+
+    def count(self, point: List[int]) -> int:
+        x,y = point
+        ans = 0
+        for (px,py) in self.points:
+            if abs(px-x) != abs(py-y) or px==x:
+                continue
+            p1 = (px, y)
+            p2 = (x, py)
+            ans += (self.points[(px,py)]*self.points.get(p1,0)*self.points.get(p2,0)) 
+        return ans
