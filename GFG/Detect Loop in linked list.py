75f541f5661aad671356fba176fed04e9a09444d
Sanjay N <sanjayn29.aiml@gmail.com>
2026-09-09 08:08:59 +0530

    Linked List Cycle Detection

---
+++ GFG/Detect Loop in linked list.py
@@ -0,0 +1,20 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def detectLoop(self, head):
+        # code here
+        if head is None:
+            return False
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
